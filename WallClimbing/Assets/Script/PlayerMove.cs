69de5a8b1ecc29c158f65add1a78e685f9fad3ec
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-12-03 10:16:54 +0900

    ClimをPlayerの方に移動

--- WallClimbing/Assets/Script/PlayerMove.cs
+++ WallClimbing/Assets/Script/PlayerMove.cs
@@ -15,7 +15,8 @@ public class PlayerMove : MonoBehaviour
     [Header("壁登りのセッティング")]
     public string wallName = "Wall";
     const float rayDistance = 0.5f;
-    public float impalseSpeed = 1.5f;
+    public float climeSpeed = 1.5f;
+    bool Climbing = false;
 
     RaycastHit _hit;
     Ray _ray;
@@ -41,27 +42,43 @@ public class PlayerMove : MonoBehaviour
 
     void Move()
     {
+        WallClim();
+
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
 
         var velocity = new Vector3(horizontal , 0 , vertical).normalized;
 
         rb.velocity = velocity * _moveSpeed;
-
         Debug.Log(rb.velocity);
+
+        // != nullしてるのは、wallNameだけだと、wallNameが見つかるまでNullが出るため。
+        //とりあえず当たっていればNullは出ないから書いた。
+        if(_hit.collider != null && _hit.collider.CompareTag(wallName))
+        {
+
+            Climbing = true;
+            velocity = new Vector3(horizontal ,vertical , 0);
+            rb.velocity = velocity * climeSpeed ;
+        }
+        else
+        {
+            transform.position = new Vector3(horizontal,vertical,0);
+            Climbing = false;
+            velocity = new Vector3(horizontal, 0, vertical).normalized;
+            rb.velocity = velocity * _moveSpeed;
+        }
     }
     /// <summary>
     /// 壁登り
     /// </summary>
     void WallClim()
     {
-        var gravity = Physics.gravity;
-
         _ray = new Ray(transform.position, transform.forward * rayDistance);
 
         if (Physics.Raycast(_ray, out _hit, rayDistance))
         {
-            if (_hit.collider.CompareTag(wallName))
+            if (_hit.collider.name ==wallName)
             {
                 rb.useGravity = false;
                 Debug.Log("I'm on the wall.");
@@ -75,18 +92,6 @@ public class PlayerMove : MonoBehaviour
             }
         }
 
-        //壁登り
-
-        Vector3 firstTransform = Vector3.up * impalseSpeed;
-
-        if (_hit.collider.CompareTag(wallName) && Input.GetKey(KeyCode.LeftShift))
-        {
-            rb.AddForce(firstTransform, ForceMode.Impulse);
-        }
-        else if (_hit.collider.CompareTag(wallName) && Input.GetKey(KeyCode.RightShift))
-        {
-            rb.AddForce(-firstTransform, ForceMode.Impulse);
-        }
-
+        Move();
     }
 }
